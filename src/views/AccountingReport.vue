765f41e5c97419e22641298cd67e7e2298e2dcb3
yuhexiong <yuhexiong871210@gmail.com>
2023-11-10 16:20:51 +0800

    feat: report table

--- src/views/AccountingReport.vue
+++ src/views/AccountingReport.vue
@@ -27,9 +27,49 @@
 
         <button @click="fetchReport">Search</button>
       </div>
+      <p></p>
 
       <div v-if="report">
-        <pre>{{ report }}</pre>
+        <table>
+          <thead>
+            <tr>
+              <th colspan="2">項目</th>
+              <th>詳細資訊</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td colspan="2">年份</td>
+              <td>{{ report.year }}</td>
+            </tr>
+            <tr>
+              <td colspan="2">月份</td>
+              <td>{{ report.month }}</td>
+            </tr>
+            <template v-if="Object.keys(report.content).length > 0">
+              <tr :key="Object.keys(report.content)[0]">
+                <td :rowspan="Object.keys(report.content).length">細項</td>
+                <td>{{ Object.keys(report.content)[0] }}</td>
+                <td>{{ report.content[Object.keys(report.content)[0]] }}</td>
+              </tr>
+            </template>
+            <template
+              v-for="(item, index) in Object.entries(report.content)"
+              :key="item"
+            >
+              <tr v-if="index !== 0">
+                <td>{{ item[0] }}</td>
+                <td>{{ item[1] }}</td>
+              </tr>
+            </template>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colspan="2">總金額</td>
+              <td>{{ report.totalAmount }}</td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
     </div>
     <div class="right-pane">
@@ -66,14 +106,17 @@ export default {
       availableYears: [2022, 2023, 2024, 2025],
       availableMonths: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
       // setting for pie chart
-      series: [44, 55, 13, 43, 22],
+      series: [],
       chartOptions: {
         chart: {
           width: 380,
           type: "pie",
+          animations: {
+            enabled: false,
+          },
         },
         colors: ["#006d77", "#83c5be", "#edf6f9", "#ffddd2", "#e29578"],
-        labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+        labels: [],
         responsive: [
           {
             breakpoint: 480,
@@ -96,7 +139,12 @@ export default {
         const response = await axiosInstance.get(
           `/report/${this.year}/${this.month}`
         );
-        this.report = await response.data;
+        this.report = response.data ?? {};
+        this.chartOptions = {
+          ...this.chartOptions,
+          labels: Object.keys(this.report?.content) ?? [],
+        };
+        this.series = Object.values(this.report?.content) ?? [];
       } catch (error) {
         console.error("Error fetching report:", error);
       }
