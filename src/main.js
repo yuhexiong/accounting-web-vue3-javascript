8e82558819084b14970a5badf52b006930befaa1
yuhexiong <yuhexiong871210@gmail.com>
2023-11-12 10:30:23 +0800

    fix: html not full height

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 
+import "./assets/allStyle.css";
+
 const app = createApp(App);
 app.use(router);
 
